d4a7a2e0161e8862e36fe4b1ad9fe9bfa205f92e
Aduardo <eduardo-ramirez97@hotmail.com>
2023-06-01 19:46:35 +0200

    he creado un punto mp3

--- ola.py
+++ ola.py
@@ -1,8 +1,12 @@
 olaaaaa!!!
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 lidia
 =======
 KLK esto es una modificación de un fichero, jijiji
 >>>>>>> 774010a (he hecho varios cambios en el fichero)
+=======
+Aloha
+>>>>>>> f3cee25 (he hecho varios cambios en el fichero)
